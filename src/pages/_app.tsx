ebbe708c5d0f6d16d931a3ed8c7e871b6440688b
Nathan Batista <70603615+NathanOasis@users.noreply.github.com>
2021-08-22 23:33:10 -0300

    created auth with github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import {AppProps} from 'next/app';
 import Head from 'next/head';
-
 import { Header } from '../components/Header';
+import { Provider as NextAuthProvider } from 'next-auth/client'
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Head>
         <title>ig.news</title>
       </Head>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
